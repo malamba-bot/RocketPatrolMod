fbc347ab3ffa2e30ed4a06bb2a84c9908f4b2e6d
Max Kinet <maxkinet@gmail.com>
2026-02-02 16:16:57 -0800

    tweak debris prefab

--- src/prefabs/Debris.js
+++ src/prefabs/Debris.js
@@ -1,13 +1,19 @@
 class Debris extends Phaser.Physics.Arcade.Sprite {
     constructor(scene, x, y) {
-        super(scene, x, y, 'ship');
+        super(scene, x, y, 'spaceship');
         this.scene = scene;
         // Add to the physics system
+        scene.add.existing(this);
         scene.physics.add.existing(this);
         // Eject the debris in a random direction at a random velocity
         this.direction = Math.random() < 0.5 ? 1 : -1;
-        this.setVelocityX(Phaser.Math.Between(75, 200) * this.direction);
+        this.setVelocityX(Phaser.Math.Between(50, 100) * this.direction);
         this.setGravityY(200);
     }
 
+    update() {
+        if (this.y <= borderUISize * 3 + borderPadding) {
+            this.destroy();
+        }
+    }
 }
